37dc7a54bdda013a50b6f96a72e3a8425a494757
Abdullah Muhammad Farouk Hakim <amf.akim@gmail.com>
2021-12-02 16:58:35 +0700

    coba landingpage apollo

--- server/index.js
+++ server/index.js
@@ -1,48 +1,35 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
 const { MONGODB } = require("./config.js");
-const User = require("./models/User");
+const typeDefs = require("./graphql/TypeDefs");
+const resolvers = require("./graphql/resolvers");
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} = require("apollo-server-core");
 
-const typeDefs = gql`
-  type User {
-    id: ID!
-    username: String!
-    password: String!
-    email: String
-    name: String
-    createdAt: String
-  }
-  type Query {
-    Hello: String!
-    getUsers: [User!]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    Hello: (parent, args, context) => "zaa warudoo",
-
-    async getUsers() {
-      try {
-        const users = await User.find();
-        return users;
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-  },
-};
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [
+    // Install a landing page plugin based on NODE_ENV
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageLocalDefault({
+          graphRef: "my-graph-id@my-graph-variant",
+          footer: false,
+        })
+      : ApolloServerPluginLandingPageProductionDefault({ footer: false }),
+  ],
 });
 
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("mongodb connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`server running at ${res.url}`);
